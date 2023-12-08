58591fabe61d74af97abf6a0b89d0b9155438c91
AndyOY647 <60327435+AndyOY647@users.noreply.github.com>
2023-12-08 10:22:52 -0500

    Background fixed more sound effect

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ pygame.init()
 pygame.mixer.init()
 row_clear_sound = pygame.mixer.Sound('Balloon_pop.mp3')
 pygame.mixer.music.load('Melody_energy.mp3')
+gameOver = pygame.mixer.Sound('game_over.wav')
+yay = pygame.mixer.Sound('Yay.mp3')
 
-main_game_bg = pygame.image.load('background.gif')
 
+window_img = pygame.image.load('window.png')
+window_img = pygame.transform.scale(window_img, (200,200))
+firework = pygame.image.load('firework.jpg')
+firework = pygame.transform.scale(firework, (1000,768))
 
 
 # creating the data structure for pieces
@@ -33,8 +38,12 @@ play_width = 640  # meaning 300 // 10 = 30 width per block
 play_height = 640  # meaning 600 // 20 = 20 height per block
 block_size = 64
 
+
+
 top_left_x = (s_width - play_width) // 2
 top_left_y = s_height - play_height
+
+
 
 # SHAPE FORMATS
 S = [[
@@ -94,6 +103,11 @@ shape_images = ['Red_Sprite.png','green_sprite.png','yellow_sprite.png','blue_sp
 
 
 
+
+    # pygame.transform.scale(bg_images[image], (1000,1000))
+
+
+
 # index 0 - 3 represent shape
 # 4 shapes
 
@@ -115,7 +129,12 @@ class Piece(object):
         surface.blit(image)
 
 
+def get_image(sheet, frame, width, height):
+    image = pygame.Surface((width, height)).convert_alpha()
+    image.blit(sheet, (0,0), ((frame*width), 0, width,height))
+    image = pygame.transform.scale(image,  (s_width,s_height))
 
+    return image
 def create_grid(locked_positions={}):
     # creates a black 10 by 10 grid
     grid = [[(0,0,0) for i in range(10)]for i in range(10)]
@@ -127,7 +146,12 @@ def create_grid(locked_positions={}):
                 grid[row][col] = c
     return grid
 
-def convert_shape_format(shape, surface):
+
+        # surface.blit(loaded , (0,0))
+
+
+
+def convert_shape_format(shape):
     positions = []
     # Getting the sublist for each rotation
     format = shape.shape[shape.rotation % len(shape.shape)]
@@ -147,16 +171,16 @@ def convert_shape_format(shape, surface):
         # offset shape left and up
         positions[i] = pos[0] - 2, pos[1] - 4
 
-    surface.blit(shape.image, positions[i])
+   
     return positions
 
 
 
 
-def valid_space(shape, grid, surface):
+def valid_space(shape, grid):
     accepted_positions = [[(j, i) for j in range(10) if grid[i][j] == (0, 0, 0)] for i in range(10)]
     accepted_positions = [j for sub in accepted_positions for j in sub]
-    formatted = convert_shape_format(shape, surface)
+    formatted = convert_shape_format(shape)
     for pos in formatted:
         if pos not in accepted_positions:
             if pos[1] > -1:
@@ -174,7 +198,7 @@ def check_lost(positions):
 
 
 def get_shape():
-    global shapes, shape_colors, shape_images
+    global shapes, shape_colors
     c_shape = Piece(3,0, random.choice(shapes))
     return c_shape
 
@@ -211,13 +235,13 @@ def clear_rows(grid, locked):
             ind = i
             for j in range(len(row)):
                 try:
-                    pygame.mixer.music.stop()
+                    pygame.mixer.music.pause()
                     row_clear_sound.set_volume(0.05)
                     pygame.mixer.Sound.play(row_clear_sound)
+                    pygame.mixer.music.play(-1)
                     del locked[(j,i)]
                 except:
-                    pygame.mixer.music.set_volume(0.5)
-                    pygame.mixer.music.play(-1)
+
                     continue
     #Shifting the row
     # if inc > 0:
@@ -234,40 +258,42 @@ def clear_rows(grid, locked):
 
 
 def draw_next_shape(shape, surface):
-    font = pygame.font.SysFont('comicsans', 10)
-    label = font.render('Next Shape', 1, (255,255,255))
+    font = pygame.font.SysFont('comicsans', 18)
+    label = font.render('Next Shape', 1, (0,0,0))
 
     sx = top_left_x +play_width
     sy = top_left_y + play_height/2 -100
 
     format = shape.shape[shape.rotation % len(shape.shape)]
 
+    surface.blit(window_img, (sx-10, sy-20))
     for i, line in enumerate(format):
         row = list(line)
         for j, column in enumerate(row):
             if column == '0':
                 #Draw image in the shape form then shift to the side of the screen
                 #pygame.draw.rect(surface, shape.color, (sx + j*block_size/2, sy +i *block_size/2, block_size/2, block_size/2), 0)
+
                 surface.blit(shape.image,(sx + j*block_size/2, sy+i *block_size/2, block_size/2, block_size/2))
-    surface.blit(label, (sx+70, sy))
+    surface.blit(label, (sx+55, sy+20))
+
 
 
 
 def draw_window(surface, grid, shape):
     row = 10
     col = 10
+    p_list = []
     #currentImage
     c_image = shape.image
+    p_list.append(c_image)
 
 
-
-
-    surface.blit(main_game_bg, (-300,0))
     pygame.font.init()
     font = pygame.font.SysFont('Grand9K Pixel', 50)
-    label = font.render('Compactor', 1, (255, 255, 255))
+    title = pygame.image.load("Title.png")
 
-    surface.blit(label, (top_left_x + play_width / 2 - (label.get_width() / 2), 30))
+    surface.blit(title, (top_left_x + play_width / 2 - (title.get_width() / 2), -70))
 
     for i in range(len(grid)):
         for j in range(len(grid[i])):
@@ -282,31 +308,48 @@ def draw_window(surface, grid, shape):
             #     previous_image = c_image
             #     surface.blit(previous_image, (top_left_x + j * block_size, top_left_y + i * block_size, block_size, block_size))
     # boarder for grid
-    pygame.draw.rect(surface, (255, 0, 0), (top_left_x, top_left_y, play_width, play_height), 4)
+            pygame.draw.rect(surface, (255, 0, 0), (top_left_x, top_left_y, play_width, play_height), 4)
 
     
     draw_grid(surface, row, col)
     #pygame.display.update()
 
-def draw_lock_pos(surface, image, pos):
-    surface.blit(image, pos)
+
 def main(win):
     lock_positions = {}
     grid = create_grid(lock_positions)
     change_piece = False
     run = True
     current_piece = get_shape()
-    previous_piece = []
-    previous_piece.append(current_piece)
     next_piece = get_shape()
     clock = pygame.time.Clock()
     fall_time = 0
     fall_speed = 0.40
     current_piece.x = 4
     level_time = 0
-    p_image = []
+    p_list = []
+    piece_in_pList = 0
+    bg_animation = []
+    animation_steps = 8
+    last_update = pygame.time.get_ticks()
+    cooldown = 500
+    frame = 0
+    win.fill((0, 0, 0))
+    for x in range(animation_steps):
+        bg_animation.append(get_image(bg_image, x, 500, 1000))
+
+
     while run:
-        win.blit(main_game_bg, (200, 0))
+        current_time = pygame.time.get_ticks()
+        if current_time - last_update >= cooldown:
+            frame += 1
+            last_update = current_time
+            if frame >= len(bg_animation):
+                frame = 0
+
+        win.blit(bg_animation[frame], (0, 0))
+
+        # bg_animation(bg_images, win)
         grid = create_grid(lock_positions)
         fall_time += clock.get_rawtime()
         clock.tick()
@@ -320,10 +363,9 @@ def main(win):
         if fall_time /1000 > fall_speed:
             fall_time = 0
             current_piece.y += 1
-            if not(valid_space(current_piece, grid, win) and current_piece.y > 0):
+            if not(valid_space(current_piece, grid) and current_piece.y > 0):
                 current_piece.y -= 1
                 change_piece = True
-                previous_piece.append(current_piece)
 
 
         #Key press check
@@ -336,29 +378,29 @@ def main(win):
                 if event.key == pygame.K_LEFT:
                     current_piece.x -= 1
                     #check boarder
-                    if not(valid_space(current_piece, grid, win)):
+                    if not(valid_space(current_piece, grid)):
                         current_piece.x += 1
                 #Right
                 if event.key == pygame.K_RIGHT:
                     current_piece.x += 1
-                    if not (valid_space(current_piece, grid, win)):
+                    if not (valid_space(current_piece, grid)):
                         current_piece.x -= 1
                 #Down
                 if event.key == pygame.K_DOWN:
                     current_piece.y += 1
-                    if not(valid_space( current_piece,grid, win)):
+                    if not(valid_space(current_piece,grid)):
                         current_piece.y -= 1
                 #UP
                 if event.key == pygame.K_UP:
                     current_piece.rotation += 1
-                    if not (valid_space( current_piece, grid, win)):
+                    if not (valid_space(current_piece, grid)):
                         current_piece.rotation -= 1
 
-        shape_pos = convert_shape_format(current_piece, win)
+        shape_pos = convert_shape_format(current_piece)
         for i in range(len(shape_pos)):
             x, y = shape_pos[i]
             if y > -1: #If we are not at the top of the grid
-                grid[y][x] = previous_piece[-1].color #update the color value
+                grid[y][x] = current_piece.color #update the color value
 
 
 
@@ -367,9 +409,12 @@ def main(win):
 
 
         if change_piece:
+            p_list.append(current_piece.image)
             for pos in shape_pos:
                 p = (pos[0], pos[1])
                 lock_positions[p] = current_piece.color
+
+
             current_piece = next_piece
             next_piece = get_shape()
             current_piece.x = 4
@@ -382,6 +427,10 @@ def main(win):
         pygame.display.update()
         if check_lost(lock_positions):
             run = False
+            pygame.mixer.music.stop()
+            gameOver.set_volume(0.1)
+            pygame.mixer.Sound.play(gameOver)
+            pygame.time.delay(4000)
 
     pygame.display.quit()
 
@@ -405,12 +454,16 @@ def main_menu(win):
 
 
         if credit_button.draw():
+
             win.blit(credit2, (300,500))
             win.blit(start1, (300,300))
-            win.fill((0,0,0))
-            win.blit(credits, (300,300))
+            win.blit(firework, (0, 0))
+            win.blit(credits, (250,100))
+            pygame.mixer.music.stop()
+            yay.set_volume(0.1)
+            pygame.mixer.Sound.play(yay)
             pygame.display.update()
-            pygame.time.delay(2000)
+            pygame.time.delay(5000)
 
 
         pygame.display.update()
@@ -445,16 +498,17 @@ class Button():
 
 win = pygame.display.set_mode((s_width, s_height))
 #bg and button
+bg_image = pygame.image.load('bg_image.png').convert_alpha()
 menu_bg = pygame.image.load('BGMenu.png').convert_alpha()
 start1 = pygame.image.load('Start1.png').convert_alpha()
 start2 = pygame.image.load('Start2.png').convert_alpha()
 credit1 = pygame.image.load('Credits1.png').convert_alpha()
 credit2 = pygame.image.load('Credits2.png').convert_alpha()
 credits = pygame.image.load('credits.png').convert_alpha()
-credits = pygame.transform.scale(credits, (300,300))
+credits = pygame.transform.scale(credits, (500,500))
 
 credit_button = Button(300,500, credit1, 1)
 start_button = Button(300,300, start1, 1)
 
-pygame.display.set_caption('KillTrash')
+pygame.display.set_caption('Compactor')
 main_menu(win)  # start game
